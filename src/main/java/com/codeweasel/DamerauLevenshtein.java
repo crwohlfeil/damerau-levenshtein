39f9b9a13f600eb154d0a8e0892a5863f6ed1336
Craig Wohlfeil <craig@luminatehealth.com>
2016-01-23 17:52:28 -0600

    Initial project structure

---
+++ src/main/java/com/codeweasel/DamerauLevenshtein.java
@@ -0,0 +1,10 @@
+package com.codeweasel;
+
+/**
+ * Calculates string distance using the Damerau-Levenshtein algorithm.
+ */
+public class DamerauLevenshtein {
+    public int calculateDistance(String source, String target) {
+        return -1;
+    }
+}
