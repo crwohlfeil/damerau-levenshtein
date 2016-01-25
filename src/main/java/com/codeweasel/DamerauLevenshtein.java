f4199a658ae9024510deb6ad5253cb5a9b4560f9
Craig Wohlfeil <crwohlfeil@gmail.com>
2016-01-25 11:15:42 -0600

    Initial implementation and tests

--- src/main/java/com/codeweasel/DamerauLevenshtein.java
+++ src/main/java/com/codeweasel/DamerauLevenshtein.java
@@ -1,10 +1,42 @@
 package com.codeweasel;
 
-/**
- * Calculates string distance using the Damerau-Levenshtein algorithm.
- */
 public class DamerauLevenshtein {
-    public int calculateDistance(String source, String target) {
-        return -1;
+    /**
+     * Calculates the string distance between source and target strings using
+     * the Damerau-Levenshtein algorithm. The distance is case-sensitive.
+     *
+     * @param source The source String.
+     * @param target The target String.
+     * @return The distance between source and target strings.
+     * @throws IllegalArgumentException If either source or target is null.
+     */
+    public static int calculateDistance(CharSequence source, CharSequence target) {
+        if (source == null || target == null) {
+            throw new IllegalArgumentException("Parameter must not be null");
+        }
+        int sourceLength = source.length();
+        int targetLength = target.length();
+        if (sourceLength == 0) return targetLength;
+        if (targetLength == 0) return sourceLength;
+        int[][] dist = new int[sourceLength + 1][targetLength + 1];
+        for (int i = 0; i < sourceLength + 1; i++) {
+            dist[i][0] = i;
+        }
+        for (int j = 0; j < targetLength + 1; j++) {
+            dist[0][j] = j;
+        }
+        for (int i = 1; i < sourceLength + 1; i++) {
+            for (int j = 1; j < targetLength + 1; j++) {
+                int cost = source.charAt(i - 1) == target.charAt(j - 1) ? 0 : 1;
+                dist[i][j] = Math.min(Math.min(dist[i - 1][j] + 1, dist[i][j - 1] + 1), dist[i - 1][j - 1] + cost);
+                if (i > 1 &&
+                        j > 1 &&
+                        source.charAt(i - 1) == target.charAt(j - 2) &&
+                        source.charAt(i - 2) == target.charAt(j - 1)) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i - 2][j - 2] + cost);
+                }
+            }
+        }
+        return dist[sourceLength][targetLength];
     }
 }
--- src/test/java/com/codeweasel/TestDamerauLevenshtein.java
+++ src/test/java/com/codeweasel/TestDamerauLevenshtein.java
@@ -2,9 +2,42 @@ package com.codeweasel;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestDamerauLevenshtein {
     @Test
     public void testCalculateDistance() {
+        assertEquals(4, DamerauLevenshtein.calculateDistance("", "test"));
+        assertEquals(4, DamerauLevenshtein.calculateDistance("test", ""));
+        assertEquals(0, DamerauLevenshtein.calculateDistance("", ""));
+        assertEquals(0, DamerauLevenshtein.calculateDistance(" ", " "));
+        assertEquals(1, DamerauLevenshtein.calculateDistance("", " "));
+        assertEquals(1, DamerauLevenshtein.calculateDistance(" ", ""));
+        assertEquals(0, DamerauLevenshtein.calculateDistance("test", "test"));
+        assertEquals(1, DamerauLevenshtein.calculateDistance("Test", "test"));
+        assertEquals(1, DamerauLevenshtein.calculateDistance("test", "testy"));
+        assertEquals(1, DamerauLevenshtein.calculateDistance("testy", "test"));
+        assertEquals(3, DamerauLevenshtein.calculateDistance("test", "testing"));
+        assertEquals(1, DamerauLevenshtein.calculateDistance("test", "tets"));
+        assertEquals(2, DamerauLevenshtein.calculateDistance("test", "tsety"));
+        assertEquals(3, DamerauLevenshtein.calculateDistance("Test", "tsety"));
+        assertEquals(1, DamerauLevenshtein.calculateDistance("test", "test "));
+        assertEquals(4, DamerauLevenshtein.calculateDistance("file", "test"));
+        assertEquals(5, DamerauLevenshtein.calculateDistance("file", "testy"));
+    }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testSourceNull() {
+        DamerauLevenshtein.calculateDistance(null, "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTargetNull() {
+        DamerauLevenshtein.calculateDistance("", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSourceAndTargetNull() {
+        DamerauLevenshtein.calculateDistance(null, null);
     }
 }
